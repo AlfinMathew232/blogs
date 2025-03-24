3f5315586a9a4db9824128c3efe81febbca37b30
Alfin232 <alfinmathew232@gmail.com>
2025-03-24 18:54:10 +0530

    Blog Updated

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alfinmathew232.pythonanywhere.com']
 
 
 # Application definition
