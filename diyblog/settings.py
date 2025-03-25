fca2fd4956e43a8121227e79bdac8a7937f1c44b
Alfin Mathew <alfinmathew232@gmail.com>
2025-03-25 10:14:49 +0530

    3Rd Uppdate

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alfinmathew232.pythonanywhere.com']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
